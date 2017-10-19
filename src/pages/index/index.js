13c09084d2e17237f6fadf12066455cfa34628af
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-10-19 11:14:44 -0500

    Added raginUp function which increases rage whenever used

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -13,7 +13,10 @@ function Gladiator(health, rage, damageLow, damageHigh) {
 
 const player1 = new Gladiator(100, 0, 12, 24);
 const player2 = new Gladiator(100, 0, 9, 27);
-var whoseTurn = 1;
+STATE = {
+    whoseTurn: 1,
+    points: ''
+};
 
 // Does the basic attack move
 // Makes the attacker have a
@@ -23,9 +26,11 @@ function attack(attacker, defender) {
     if (Math.floor(Math.random() * 100) + 1 <= attacker.rage) {
         defender.health = defender.health - hit * 2;
         attacker.rage = 0;
+        STATE.points = 'Crit hit of ' + hit * 2 + ' Damage!';
     } else {
         attacker.rage += 15;
         defender.health = defender.health - hit;
+        STATE.points = 'Regular hit of ' + hit + ' Damage!';
     }
     if (defender.health <= 0) {
         defender.health = 0;
@@ -35,6 +40,7 @@ function attack(attacker, defender) {
 function heal(attacker) {
     if (attacker.rage >= 10) {
         attacker.health = attacker.health += 5;
+        STATE.points = 'Player healed 5 points.';
         if (attacker.health > 100) {
             attacker.health = 100;
         }
@@ -43,6 +49,10 @@ function heal(attacker) {
             attacker.rage = 0;
         }
     }
+}
+
+function ragingUp(attacker) {
+    attacker.rage += 15;
 }
 
 function isDead(attacker) {
@@ -54,7 +64,7 @@ function isDead(attacker) {
 }
 // *********************************
 function combater() {
-    if (whoseTurn === 1) {
+    if (STATE.whoseTurn === 1) {
         return player1;
     } else {
         return player2;
@@ -62,7 +72,7 @@ function combater() {
 }
 
 function defending() {
-    if (whoseTurn === 1) {
+    if (STATE.whoseTurn === 1) {
         return player2;
     } else {
         return player1;
@@ -70,15 +80,16 @@ function defending() {
 }
 
 function changeTurn() {
-    if (whoseTurn === 1) {
-        whoseTurn = 2;
+    if (STATE.whoseTurn === 1) {
+        STATE.whoseTurn = 2;
     } else {
-        whoseTurn = 1;
+        STATE.whoseTurn = 1;
     }
 }
 
 function reload(attacker, defender) {
-    whoseTurn = 1;
+    STATE.points = '';
+    STATE.whoseTurn = 1;
     attacker.health = 100;
     defender.health = 100;
     attacker.rage = 0;
@@ -87,14 +98,11 @@ function reload(attacker, defender) {
 
 function attatchHandlers() {
     $('#attack').click(function() {
-        isDead(combater());
         attack(combater(), defending());
         changeTurn();
         draw();
     });
     $('#heal').click(function() {
-        viewableHealButton(combater());
-        isDead(combater());
         heal(combater());
         changeTurn();
         draw();
@@ -105,17 +113,35 @@ function attatchHandlers() {
     });
 }
 
-function viewableHealButton(player) {
-    if (player.rage >= 10) {
-        return ["<button id='heal'>Heal</button></div>"].join('');
+// function viewableHealButton(player) {
+//     if (player.rage >= 10) {
+//         return ["<button id='heal'>Heal</button></div>"].join('');
+//     }
+// }
+
+function viewableButtons(player) {
+    able = [];
+    if (isDead(player) == true) {
+        able.push('');
     }
+    if (player.rage >= 10 && player.health) {
+        able.push("<button id='heal'>Heal</button>");
+    }
+    if (player.health > 0) {
+        able.push("<button id='attack'>Attack</button>");
+    }
+    return able.join('');
 }
 
 function buttonView() {
     return [
-        "<div><button id='attack'>Attack</button>",
-        viewableHealButton(combater()),
+        STATE.points,
+        '<div>',
+        viewableButtons(combater()),
         '</div>',
+        // "<div><button id='attack'>Attack</button>",
+        // viewableHealButton(combater()),
+        // '</div>',
         "<div><button id='restart'>Restart</button></div>"
     ].join('');
 }
